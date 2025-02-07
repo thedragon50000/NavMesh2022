d2a74b5a9618f0674bd989eba29cea0bf2abe40a
Hao <s1810942008@gms.nutc.edu.tw>
2025-02-07 18:50:39 +0800

    War Scene Init

---
+++ Assets/Andy/Scripts/Characters/baseCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+using UniRx;
+
+namespace Andy.Scripts.Characters
+{
+    public class baseCharacter : MonoBehaviour
+    {
+        private float _atk;
+        private float _hp;
+        private float _speed;
+
+        [Inject] public Transform _goal;
+
+        NavMeshAgent _nav;
+
+        private void Awake()
+        {
+            _nav = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            _nav.SetDestination(_goal.position);
+        }
+    }
+}
